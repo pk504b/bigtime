192e470c5acf6e4365572feea0f088b8daec940a
pk504b <pk.504b@gmail.com>
2025-03-28 20:58:29 +0100

    back to original

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,16 +7,6 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
-
-		interface Platform {
-			env: {
-				COUNTER: DurableObjectNamespace;
-			};
-			context: {
-				waitUntil(promise: Promise<any>): void;
-			};
-			caches: CacheStorage & { default: Cache }
-		}
 	}
 }
 
