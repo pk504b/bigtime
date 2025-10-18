8a0eafe5496e3c3d43239e434665ed9b5fdd725b
Prince <pk.504b@gmail.com>
2025-10-18 19:01:51 +0200

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Navigation from "@/components/Navigation";
 import { Rubik } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://bigtime.pw"),
