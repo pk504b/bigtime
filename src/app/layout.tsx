c9c2d0da091d207e047b4fca1e92ec2fc28404b1
Prince <pk.504b@gmail.com>
2026-01-03 12:25:56 +0100

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import Navigation from "@/components/Navigation";
 import { Rubik } from "next/font/google";
 import { baseMetadata } from "@/lib/metadata";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata = baseMetadata;
 
@@ -18,6 +19,7 @@ export default function RootLayout({
       <head>
         <meta name="apple-mobile-web-app-title" content="Bigtime" />
       </head>
+      <GoogleAnalytics gaId="G-VH8F8CHWBM" />
       <body
         className={`${rubik.className} h-screen flex flex-col bg-hintofyellow text-midnight dark:bg-midnight dark:text-lightbrigtgreen`}
       >
